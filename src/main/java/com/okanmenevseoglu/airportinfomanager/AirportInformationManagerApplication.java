88e0714e43e68a05e30a719e3d1c1a8be373ac90
Okan Menevseoglu <okan.menevseoglu@dolap.com>
2018-06-24 12:50:55 +0300

    Initial commit

---
+++ src/main/java/com/okanmenevseoglu/airportinfomanager/AirportInformationManagerApplication.java
@@ -0,0 +1,12 @@
+package com.okanmenevseoglu.airportinfomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportInformationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportInformationManagerApplication.class, args);
+    }
+}
